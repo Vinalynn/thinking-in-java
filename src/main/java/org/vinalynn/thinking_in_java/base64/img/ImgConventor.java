01f0c6b8d8f53c426757f9836aef42bfc2f0c21a
Vinalynn <william.cai@live.com>
2013-05-14 12:23:48 +0800

    添加单元测试框架

---
+++ src/main/java/org/vinalynn/thinking_in_java/base64/img/ImgConventor.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package org.vinalynn.thinking_in_java.base64.img;
+
+import org.vinalynn.thinking_in_java.common.Task;
+
+/**
+ * @author caiwm
+ *
+ */
+public class ImgConventor extends Task{
+
+	
+	
+	
+	
+	
+	public void doTask() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+
+}
--- src/test/java/org/vinalynn/thinking_in_java/AppTest.java
+++ src/test/java/org/vinalynn/thinking_in_java/AppTest.java
@@ -1,38 +1,69 @@
 package org.vinalynn.thinking_in_java;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import java.lang.reflect.InvocationTargetException;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+import org.apache.commons.lang3.reflect.MethodUtils;
+import org.junit.Before;
+import org.junit.Test;
+import org.vinalynn.thinking_in_java.base64.img.ImgConventor;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+public class AppTest {
+	
+	private HashMap<String, Integer> testStore; 
+	private Iterator<Map.Entry<String, Integer>> entris;
+	@Before
+	public void setUp(){
+		testStore = new HashMap<String, Integer>();
+		
+		//define tester
+		//1-process 0-ignore
+		testStore.put(ImgConventor.class.getName(), 1);
+		
+		
+		entris = testStore.entrySet().iterator();
+	}
+	
+	@Test
+	public void testApp() {
+		Map.Entry<java.lang.String, java.lang.Integer> entry;
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+		while (entris.hasNext()) {
+			entry = (Map.Entry<java.lang.String, java.lang.Integer>) entris
+					.next();
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+			if (entry.getValue() == 1) {
+				System.out.println("testing " + entry.getKey());
+				try {
+					MethodUtils.invokeMethod(Class.forName(entry.getKey())
+							.newInstance(), "doTask", new Object[] {});
+				} catch (NoSuchMethodException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IllegalAccessException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (InvocationTargetException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (ClassNotFoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (InstantiationException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			} else {
+				System.out.println("test class[" + entry.getKey()
+						+ " was ignored.");
+			}
+
+		}
+
+	}
 }
