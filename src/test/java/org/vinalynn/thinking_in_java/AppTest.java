23a526e309694deeae2a902085c4440f00b167c8
Vinalynn <william.cai@live.com>
2013-05-14 13:16:14 +0800

    optimize imports

--- src/test/java/org/vinalynn/thinking_in_java/AppTest.java
+++ src/test/java/org/vinalynn/thinking_in_java/AppTest.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStream;
 import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 import java.util.Iterator;
